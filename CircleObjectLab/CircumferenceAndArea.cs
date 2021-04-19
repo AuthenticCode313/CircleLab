233725b23465b7d28f705c2d71ec22a700203e3f
AuthenticCode313 <80369849+AuthenticCode313@users.noreply.github.com>
2021-04-18 20:23:36 -0700

    First commit

---
+++ CircleObjectLab/CircumferenceAndArea.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircleObjectLab
+{
+    class CircumferenceAndArea
+    {
+        private double _radius;
+
+        public double Radius
+        {
+            get { return _radius; }
+            set { _radius = value; }
+        }
+        public double Circle(double radius)
+        {
+            this._radius = radius;
+            return this._radius;
+        }
+
+
+        public double CalculateCircumference()
+        {
+            double circumference = 2 * Math.PI * this._radius;
+            return circumference;
+               
+        }
+
+        public string CalculateFormattedCircumference()
+        {
+            
+            return $"{Math.Round(CalculateCircumference(), 2)}";
+        }
+
+        public double CalculateArea()
+        {
+            double area = Math.PI * this._radius * this._radius;
+            return area;
+
+        }
+
+        public string CalculatedFormattedArea()
+        {
+           
+            return $"{Math.Round(CalculateArea(), 2)}";
+        }
+
+        private string FormatNumber(double x)
+        {
+            return $"Circumference: {CalculateFormattedCircumference()} \n Area: {CalculatedFormattedArea()}";
+        }
+    }
+}
---
+++ CircleObjectLab/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace CircleObjectLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Circle Tester!");
+            Console.WriteLine();
+            CircumferenceAndArea cal = new CircumferenceAndArea();
+
+            bool runProgram = true;
+            while (runProgram)
+            {
+                Console.WriteLine("Enter a radius: ");
+                cal.Radius = double.Parse(Console.ReadLine());
+
+                if (cal.Radius <= 0)
+                {
+                    Console.WriteLine("Invalid input. Please enter a radius greater than 0");
+                }
+                else
+                {
+                    Console.WriteLine($"Circumference: {cal.CalculateFormattedCircumference()} \n Area: {cal.CalculatedFormattedArea()}");
+                }
+
+                Console.WriteLine("Continue y/n?");
+                string loop = Console.ReadLine();
+                while (true)
+                if (loop == "y")
+                {
+                    break;
+                }
+                else if (loop == "n")
+                {
+                    Console.WriteLine($"Goodbye. You created Circle objects.");
+                    runProgram = false;
+                    break;
+                }
+            }
+
+        }
+    }
+}
